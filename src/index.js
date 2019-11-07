baf5b2cce215e51888340d1eecdea8216fd82570
clement <clement@seald.io>
2019-11-07 16:05:29 +0100

    correct usage of babel 7.6+

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { decryptOfficeFile, isOLEDoc } from './ooxml.js'
-import "@babel/polyfill"
 
 const decryptOLEDoc = async (buffer, passwordCallback) => {
   if (isOLEDoc(buffer)) return decryptOfficeFile(buffer, passwordCallback)
