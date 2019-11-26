b1a0a10d2f11248ad93e3baff7f42d22bf5b4568
arantes555 <arantes555@gmail.com>
2019-11-26 17:57:10 +0100

    optimize decryption a bit

--- src/ecma376_agile.js
+++ src/ecma376_agile.js
@@ -1,27 +1,28 @@
 import forge from 'node-forge'
 
-const hashCalc = function (salt, algorithm) {
-  if (algorithm === 'SHA512') return forge.md.sha512.create().update(salt, 'binary')
-  else return forge.md.sha1.create().update(salt, 'binary')
+const hashCalc = function (buff, algorithm) {
+  if (algorithm === 'SHA512') return Buffer.from(forge.md.sha512.create().update(buff.toString('binary')).digest().data, 'binary')
+  else return Buffer.from(forge.md.sha1.create().update(buff.toString('binary')).digest().data, 'binary')
 }
 
+const block3 = Buffer.from('FG4L56us0NY=', 'base64')
+
 export const makeKeyFromPassword = function (password, saltValue, hashAlgorithm, encryptedKeyValue, spinValue, keyBits) {
-  let hash = hashCalc(saltValue + Buffer.from(password, 'utf16le').toString('binary'), hashAlgorithm)
-  const block3 = Buffer.from('FG4L56us0NY=', 'base64').toString('binary')
+  const passwordBuffer = Buffer.from(password, 'utf16le')
+  const saltBuffer = Buffer.from(saltValue, 'binary')
+  let hash = hashCalc(Buffer.concat([saltBuffer, passwordBuffer]), hashAlgorithm)
 
   for (let i = 0; i < spinValue; i++) {
     const buff = Buffer.alloc(4)
     buff.writeUInt32LE(i)
-    hash = hashCalc(buff.toString('binary') + hash.digest().data, hashAlgorithm)
+    hash = hashCalc(Buffer.concat([buff, hash]), hashAlgorithm)
   }
 
-  const h2 = hashCalc(hash.digest().data + block3, hashAlgorithm)
-  const key = h2.digest().data.slice(0, Math.floor(keyBits / 8))
+  const h2 = hashCalc(Buffer.concat([hash, block3]), hashAlgorithm)
+  const key = h2.slice(0, Math.floor(keyBits / 8))
 
   const aes = forge.cipher.createDecipher('AES-CBC', forge.util.createBuffer(key))
-  aes.start({
-    iv: forge.util.createBuffer(saltValue.toString('binary'))
-  })
+  aes.start({ iv: forge.util.createBuffer(saltBuffer) })
   aes.update(forge.util.createBuffer(encryptedKeyValue))
   aes.finish(() => true) // Forge will not unpad if a callback return true...
   // TODO: verifier le HMAC si il est donné dans les infos
@@ -33,23 +34,23 @@ export const decrypt = function (secretKey, keyDataSalt, keyDataHashAlgorithm, b
   const SEGMENT_LENGTH = 4096
   let outputBuffer = Buffer.alloc(0)
   const newBuffer = buffer.slice(8, buffer.length)
+  const length = Math.floor(newBuffer.length / SEGMENT_LENGTH)
 
   // Parallelize chunk decryption
   // Verify HMAC (encryptedVerifierHashInput, encryptedVerifierHashValue) ?
-  for (let i = 0; i <= Math.floor(newBuffer.length / SEGMENT_LENGTH); i++) {
+  for (let i = 0; i <= length; i++) {
     const buff = Buffer.alloc(4)
     buff.writeUInt32LE(i)
-    const saltWithBlockKey = keyDataSalt + buff.toString('binary')
-    const iv = hashCalc(saltWithBlockKey, keyDataHashAlgorithm)
-    const _iv = Buffer.from(iv.digest().data, 'binary').slice(0, 16)
+    const saltWithBlockKey = Buffer.concat([Buffer.from(keyDataSalt, 'binary'), buff])
+    const iv = hashCalc(saltWithBlockKey, keyDataHashAlgorithm).slice(0, 16)
     const inter = newBuffer.slice(i * SEGMENT_LENGTH, (i + 1) * SEGMENT_LENGTH)
     const aes = forge.cipher.createDecipher('AES-CBC', forge.util.createBuffer(secretKey))
-    aes.start({ iv: forge.util.createBuffer(_iv.toString('binary')) })
-    aes.update(forge.util.createBuffer(inter.toString('binary')))
+    aes.start({ iv: forge.util.createBuffer(iv) })
+    aes.update(forge.util.createBuffer(inter))
     aes.finish(() => true) // Forge will not unpad if a callback return true...
     let dec = Buffer.from(aes.output.data, 'binary')
     if (remaining < inter.length) {
-      dec = Buffer.from(aes.output.data, 'binary').slice(0, remaining)
+      dec = dec.slice(0, remaining)
     }
     outputBuffer = Buffer.concat([outputBuffer, dec])
     remaining -= inter.length
